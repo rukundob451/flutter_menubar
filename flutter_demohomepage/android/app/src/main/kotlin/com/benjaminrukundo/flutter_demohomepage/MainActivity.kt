a4561eac9674d7230493bdff05a44d13c5614177
Benjamin Rukundo <rukundob451@gmail.com>
2023-02-10 15:52:56 +0300

    flutter_demohomepage

---
+++ flutter_demohomepage/android/app/src/main/kotlin/com/benjaminrukundo/flutter_demohomepage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.benjaminrukundo.flutter_demohomepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_demohomepage/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
